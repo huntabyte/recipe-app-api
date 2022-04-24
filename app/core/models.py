a32606e7a6ba7566ff5c43c21880bf8c8af09ec6
Hunter Johnston <johnstonhuntera@gmail.com>
2022-04-24 12:57:34 -0400

    updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -19,7 +19,7 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self, email, password):
+    def create_superuser(self, email, password):
         """Creates and saves a new super user"""
         user = self.create_user(email, password)
         user.is_staff = True
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -26,7 +26,7 @@ class ModelTests(TestCase):
 
     def test_create_new_super_user(self):
         """Test creating a new super user"""
-        user = get_user_model().objects.create_super_user(
+        user = get_user_model().objects.create_superuser(
             "test@huntabyte.com", "test123"
         )
 
