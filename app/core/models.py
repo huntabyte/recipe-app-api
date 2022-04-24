2ce3856298304a185c8cc881fed898750e4e2c37
Hunter Johnston <johnstonhuntera@gmail.com>
2022-04-24 19:48:20 -0400

    added recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -64,3 +64,19 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField("Ingredient")
+    tags = models.ManyToManyField("Tag")
+
+    def __str__(self):
+        """Returns a string representation of the recipe"""
+        return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -53,3 +53,11 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(), title="Steak and Potatoes", time_minutes=5, price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
